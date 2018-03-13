862c3eab92152f7df42bacefdde7a17c115e25c9
carol hill <carolhill@MBP-di-carol.attlocal.net>
2018-03-12 21:41:06 -0700

    More modifications.  Completed boolean parenthesis logic

--- non-type-template-args.cpp
+++ non-type-template-args.cpp
@@ -13,12 +13,27 @@ void  myfunc(char (&a)[n])
 template <>
 void  myfunc(char (&a)[4])
 {
-		cout << "specialized ... " << a << " sizeof: " << sizeof(a) << endl;
+		cout << "overloaded ... " << a << " sizeof: " << sizeof(a) << endl;
 };
+
+
+template <size_t>
+void pos_function(size_t n)
+{
+	cout << "size of n is not 3, it is " << n << endl;
+}
+
+template<>
+void pos_function<3>(size_t pos)
+{
+	cout << "size of n is 3" << endl;
+}
 
 int main () 
 { 
 	myfunc(fig);
 	myfunc(mulberry);
+	pos_function<5>(5);
+	pos_function<3>(3);
 	return 0;
 }
--- template-parenthesis-count.cpp
+++ template-parenthesis-count.cpp
@@ -1,31 +1,129 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
 using namespace std;
-template <char (&left)[], char (&right)[]> 
-void parenthesize (const char[] left, const char[] right) {
 
-	size_t left_len = sizeof(left) - sizeof (right);
+// gen_parenthesizations
+//
+// Given an expression of left-associating operators, permute the possible parenthesizations 
+//
+// eg. T^F|T&F^T
+//
+//	T^F|T&F^T
+//	T^F|T&(F^T)
+//	T^F|(T&F^T)
+//	T^F|(T&(F^T))
+//	T^F|(T&F)^T
+//	T^(F|T&F^T)
+//	T^(F|T&(F^T))
+//	T^(F|(T&F^T))
+//	T^(F|(T&(F^T)))
+//	T^(F|(T&F)^T)
+//	T^(F|T)&F^T
+//	T^(F|T)&(F^T)
+//	T^(F|T&F)^T
+//	T^(F|(T&F))^T
 
-	for (int i=0; i<left_len; i++) cout << s[i];
-	cout << '(' << right << ')' << endl;
+vector<string> gen_parenthesizations(const char * const expression)
+{
 
-	cout << '(';
-	for (int i=0; i<left_len+1; i++) cout << s[i];
-	cout << ')' << right+1 << endl;
-
-	parenthesize(left,right+2);
-}
-
-template <char (&s)[3] s> 
-void parenthesize(const char[] s){
-	cout << '('
-	for (int i=0; i<2; i++) cout << s[i];
-	cout << endl;
+	vector<string> v;
+	ostringstream os;
 	
+	if ( 2 >= strlen(expression)) {
+
+		// trailing sub-expression on the right eg. '&T' or 'T', just return that
+		os << expression ;
+		v.push_back(os.str());
+		os.str("");
+
+	} else {
+
+		const char* sub_expression = expression + strlen(expression)-3; // rightmost two operands eg. T&F
+
+		// starting from the right, working to the left, each iteration add another operand and operator ( eg. T& )
+		// to the expression. Then build the list of permuatations based on the previous iteration 
+		while (sub_expression >= expression) {
+
+			vector<string> vtemp;
+
+
+			if (3 == strlen(sub_expression)) {
+
+				// base case, just the rightmost 3 tokens eg. T^F
+				os << sub_expression ;
+				vtemp.push_back(os.str());
+				os.str("");
+
+				// same but add parenthesis 
+				os << '(' << sub_expression << ')' ;
+				vtemp.push_back(os.str());	
+				os.str("");
+
+
+			} else {
+
+				// construct all permutations with the right side, 
+				// the vector will grow exponentially each loop
+				for (vector<string>::iterator it=v.begin();it!=v.end(); ++it) {
+
+					os << string(sub_expression).substr(0,2) <<  *it ;
+					vtemp.push_back(os.str());	
+					os.str("");
+
+
+				}
+
+				// same thing again, but enclose in parenthesis
+				// no enclosing parenthesis on the entire expression however, that is extraneous 
+				// eg. (T&F|T) is the same as T&F|T
+				for (vector<string>::iterator it=v.begin();it!=v.end(); ++it) {
+					if (sub_expression==expression) break;
+
+					os << '(' << string(sub_expression).substr(0,2) << *it << ')';
+					vtemp.push_back(os.str());	
+					os.str("");
+
+
+				}
+
+				// now parenthesis on the leftmost operands eg. (T&F)|T and permute with right
+				for (size_t pos=3; pos!=string(sub_expression).length(); pos+=2)
+				{
+					string left(string(sub_expression).substr(0,pos));
+					vector<string> vleft(gen_parenthesizations(left.c_str()));
+
+					for(vector<string>::iterator it=vleft.begin(); it!=vleft.end()-1; ++it) {
+						os << '(' << *it << ')' << sub_expression[pos]; // eg. (T&F)^
+						string left(os.str());
+						os.str("");
+
+						vector<string> vright(gen_parenthesizations(sub_expression+pos+1));
+						for (vector<string>::iterator it=vright.begin(); it!=vright.end(); ++it)
+						{
+							os <<  left << *it;
+							vtemp.push_back(os.str());	
+							os.str("");
+						}
+					}
+				}
+			}
+
+			v = vtemp;
+			sub_expression -=2; // add next operand and operator on the left eg. T&
+		}
+	}
+
+	return v;
 }
 
 int main ()
 {
-	char my_expression[] = "T&F^T";
-	parenthesize(my_expression, my_expression);
+	const char my_expression[] = "T^F|T&F^T";
+	
+	vector<string> v = gen_parenthesizations(my_expression);
+	for (vector<string>::iterator it=v.begin(); it!=v.end(); ++it)  cout << *it << endl;
 	return 0;
 }
