fc602fa80c24240d43e0387c8916e4aecbf8168b
carol hill <gcross@licmax.com>
2019-09-06 19:18:06 -0700

    Added rearrange sorted array alternating problem

--- rearrange_array_alternately.cpp
+++ rearrange_array_alternately.cpp
@@ -1,10 +1,9 @@
-#include <list>
 #include <iterator>
 #include <iostream>
 #include <limits>
 #include <sstream>
 #include <string>
-#include <utility>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -15,29 +14,38 @@ int main() {
 	while (T--) {
 
 		cin >> N;
-		list<unsigned> v(N);
-		list<unsigned>::iterator it = v.begin();
+		vector<string> v(N);
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		string line;
 		getline(cin,line);
 		istringstream is(line);
+		vector<string>::iterator it = v.begin();
 		while (is && it!=v.end()) is >> *it++;
 
 		// now rearrange, alternate
-		unsigned marker = v.back(), num_loops=0;
-		while (marker != v.front()) {  
-			v.push_back(v.front());
-			v.pop_front();
-			if (marker != v.front()) {
-				list<unsigned>::iterator it = find(v.begin(), v.end(), marker); 
-				--it; 
-				v.push_back(*it);
-				v.erase(it);
-			}
+		it = v.begin();
+		vector<string>::reverse_iterator rit = v.rbegin();
+		while (it < rit.base()) {
+			*it += string("|") + *rit; 
+			++it; ++rit;
 		}
 
-		
-		for (unsigned i: v) cout << i << ' ';
+		// annoyance trying to compare reverse iterators and iterators
+		vector<string>::reverse_iterator rit2 = v.rbegin();
+		if (N%2) {
+			--it;	
+			*rit2++ = it->substr(it->find('|')+1); 
+		}
+
+		while (rit != v.rend()) {
+			*rit2 = rit->substr(0,rit->rfind('|'));
+			++rit2;
+			*rit2 = rit->substr(rit->find('|')+1);
+			++rit; ++rit2;
+			
+		}
+
+		for (string i: v) cout << i << ' ';
 		cout << endl;
 	}
 	return 0;
