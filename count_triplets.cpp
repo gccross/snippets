d53fabcf48c09f4cebb99e99eb7f24df9f29362e
carol hill <gcross@licmax.com>
2019-11-12 21:19:04 -0800

    Rewrote using coalesced longs as bit fields.  Reduces algorithm to O(n).  SegFaults on G4G but I can't see the testcase to debug.  Runs fine on smaller tests.

--- count_triplets.cpp
+++ count_triplets.cpp
@@ -1,66 +1,79 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-
+ 
 using namespace std;
+ 
+unsigned get_bitfield_section(unsigned _num)
+{
+    unsigned section = 0;
+	int num { static_cast<int>(_num) };  // need signed int for next line
+    while ((num-=64)  > -1) 
+        section++;
 
-int main(int argc, char const * const argv[]) {
-	size_t T;
-	cin >> T;
-	while (T--) {
-	    size_t N;
-	    cin >> N;
-	    int a[N];
-	    copy_n(istream_iterator<int>(cin), N, a);
-        sort (a, a+N );
-        
-        int count = 0;
-
-	    for (int i = N-2; i>=0; --i) {
-			// compare every element in the sequence against the other.
-			int len = N;
-			for (int j = i-1; j>=0; --j) {  
-
-				// short circuits
-				if (a[i] + a[j] < a[i+1] )  break; // if sum less than next after larger operand, don't waste time looking further
-				if (a[i] + a[j] > a[len-1]) { --j; continue; }  // if sum greater than greatest number, continue to reduce smaller operand
-
-				int* p = lower_bound(a+i+1, a+len, a[i] + a[j]) ; // now we're in range, so search
-				if (*p == a[i] + a[j]) 
-					++count; // increase count if found
-				len = distance(a,p); // impossible to find greater than a[i]+a[j] as we reduce j, so don't bother looking past it.
-			}
-		}
-	    
-	    cout << (count == 0 ? -1 : count) << endl;
-	}
-	return 0;
+    return section;
 }
-//Given an array of distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
-//
-//Input:
-//The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. First line of each test case contains an Integer N denoting size of array and the second line contains N space separated elements.
-//
-//Output:
-//For each test case, print the count of all triplets, in new line. If no such triplets can form, print "-1".
-//
-//Constraints:
-//1 <= T <= 100
-//3 <= N <= 105
-//1 <= A[i] <= 106
-//
-//Example:
-//Input:
-//2
-//4
-//1 5 3 2
-//3
-//3 2 7
-//Output:
-//2
-//-1
-//
-//Explanation:
-//Testcase 1: There are 2 triplets: 1 + 2 = 3 and 3 +2 = 5
-//
+int main(int argc, char const * const argv[]) {
+    size_t T;
+    cin >> T;
+    while (T--) {
+        size_t N;
+        cin >> N;
+        unsigned int a[N];
+        
+        uint64_t bits[4]; // largest sum can't be > 209 (104+105)
+		fill(bits, bits+4, 0UL);
+        size_t i{N};
+        while (i--) {
+            cin >> a[i];
+            uint64_t mask = 1UL << (a[i] - 1); // don't forget UL on the constant.  Otherwise, you wrap at 32 bits.
+            unsigned section = get_bitfield_section(a[i]);
+            bits[section] |= mask;
+        }
+        
+        int count=0;
+        for (unsigned i=0; i<N-1; ++i) {
+            for (unsigned j=i+1; j<N; ++j) {
+                
+                uint64_t sum = a[i]+a[j];
+                if (sum > 104+105) continue; // according to problem description
+                
+				uint64_t mask = 1UL << (sum - 1);
+                unsigned section = get_bitfield_section(sum);
+				
+				bool res = mask & bits[section];
+                if (res) ++count;
+            }
+        }
+        cout << (count == 0 ? -1 : count) << endl;
+    }
+    return 0;
+}
+// Given an array of distinct integers. The task is to count all the triplets such that sum of two elements equals the third element.
+// 
+// Input:
+// The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. First line of each test case contains an Integer N denoting size of array and the second line contains N space separated elements.
+// 
+// Output:
+// For each test case, print the count of all triplets, in new line. If no such triplets can form, print "-1".
+// 
+// Constraints:
+// 1 <= T <= 100
+// 3 <= N <= 105
+// 1 <= A[i] <= 106
+// 
+// Example:
+// Input:
+// 2
+// 4
+// 1 5 3 2
+// 3
+// 3 2 7
+// Output:
+// 2
+// -1
+// 
+// Explanation:
+// Testcase 1: There are 2 triplets: 1 + 2 = 3 and 3 +2 = 5
+// 
 
