810c4decc8d2857f040b5fbd8e92e08a852fe6ed
carol hill <gcross@licmax.com>
2019-11-14 12:41:19 -0800

    Eliminated pivot variable in partition function.  There is no need for it.  Just use right instead.

--- partition_sort.cpp
+++ partition_sort.cpp
@@ -6,16 +6,15 @@
 #include <vector>
 #include <utility>
 
-
 template <typename It>
 It partition(const It first,const It last)
 {
-	It left=first+1, right=last-1, mid=first;
+	It left=first+1, right=last-1;
 
 	while (left <= right)
 	{
-		while(left <= right && *left <= *mid) ++left;
-		while(left <= right && *right > *mid) --right;
+		while(left <= right && *left <= *first) ++left;
+		while(left <= right && *right > *first) --right;
 
 		if (left < right) std::swap(*left, *right);
 	}
@@ -32,7 +31,7 @@ void quicksort(It first, It last)
 	It mid = partition(first, last);
 
 	quicksort(first, mid);
-	quicksort(mid+1, last);
+	quicksort(mid+1, last); // mid is already in place
 
 }
 
@@ -49,5 +48,6 @@ int main(int argc, const char * const argv[])
 	quicksort(v.begin(), v.end());
 	for (std::string s: v) std::cout << s << " ";
 	std::cout << std::endl;
+
 	return 0;
 }
