2b83cfbe4eb3265cd44848495fd6e266b70c0dd7
carol hill <gcross@licmax.com>
2019-11-26 22:05:28 -0800

    Added two accepted problems from hackerrank: between_two_sets and breaking_the_records.  Both easy.

---
+++ breaking_the_records.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the breakingRecords function below.
+vector<int> breakingRecords(vector<int> scores) {
+    int highs{0}, lows{0}, high{*scores.begin()}, low{*scores.begin()};
+    for_each(scores.begin(), scores.end(), 
+        [&lows,&highs, &high, &low](int i){
+            if (i > high) { high = i; ++highs; return; }
+            if (i < low) { low = i; ++lows; return; }
+        });
+    
+    return vector<int>{ highs, lows };
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string scores_temp_temp;
+    getline(cin, scores_temp_temp);
+
+    vector<string> scores_temp = split_string(scores_temp_temp);
+
+    vector<int> scores(n);
+
+    for (int i = 0; i < n; i++) {
+        int scores_item = stoi(scores_temp[i]);
+
+        scores[i] = scores_item;
+    }
+
+    vector<int> result = breakingRecords(scores);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << " ";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+/*
+
+From Hackerrank.com
+
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
+For example, assume her scores for the season are represented in the array . Scores are in the same order as the games played. She would tabulate her results as follows:
+                                 Count
+Game  Score  Minimum  Maximum   Min Max
+ 0      12     12       12       0   0
+ 1      24     12       24       0   1
+ 2      10     10       24       1   1
+ 3      24     10       24       1   1
+Given Maria's scores for a season, find and print the number of times she breaks her records for most and least points scored during the season.
+Function Description
+Complete the breakingRecords function in the editor below. It must return an integer array containing the numbers of times she broke her records. Index  is for breaking most points records, and index  is for breaking least points records.
+breakingRecords has the following parameter(s):
+scores: an array of integers
+Input Format
+The first line contains an integer , the number of games. 
+The second line contains  space-separated integers describing the respective values of .
+Constraints
+
+
+Output Format
+Print two space-seperated integers describing the respective numbers of times her best (highest) score increased and her worst (lowest) score decreased.
+Sample Input 0
+9
+10 5 20 20 4 5 2 25 1
+Sample Output 0
+2 4
+Explanation 0
+The diagram below depicts the number of times Maria broke her best and worst records throughout the season:
+image
+She broke her best record twice (after games  and ) and her worst record four times (after games , , , and ), so we print 2 4 as our answer. Note that she did not break her record for best score during game , as her score during that game was not strictly greater than her best record at the time.
+Sample Input 1
+10
+3 4 21 36 10 28 35 5 24 42
+Sample Output 1
+4 0
+*/
+
