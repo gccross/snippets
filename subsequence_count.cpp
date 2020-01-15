91681fe9ebc6e98896a67856f30addf2129e7640
carol hill <gcross@licmax.com>
2020-01-15 08:45:09 -0800

    Updated comments in subsequence_count.cpp

--- subsequence_count.cpp
+++ subsequence_count.cpp
@@ -62,3 +62,32 @@ int subsequenceCount_dp(string S, string T)
     
             
 }
+
+/*
+Given two strings S and T, find count of distinct occurrences of T in S as a sub-sequence. Your task is to complete the function subsequenceCount which takes two strings as argument S and T and returns the count of the sub-sequences.
+
+Input:
+The first line of input contains an integer t denoting the no of test cases . Then t test cases follow. The first line of each test case contains 2 strings S and T.
+
+Output:
+For each test case in a new line print the count of distinct occurrences of T in S as a sub-sequence.
+
+Constraints:
+1<=t<=100
+1<=length of (S,T)<=100
+
+Example(To be used only for expected output):
+Input
+2
+banana ban
+geeksforgeeks ge
+
+Output
+3
+6
+
+Explanation:
+(i) For first test case  S = banana, T = ban there are 3 sub-sequences which are [ban], [ba n], [b an].
+(ii) For second test case S = geeksforgeeks, T=ge there are 6 sub-sequences which are  [ge], [ ge], [g e], [g e] [g e] and [ g e].
+
+*/
