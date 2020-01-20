c5a17b3c71c805ce47b1a8063916dde6f80f2c21
carol hill <gcross@licmax.com>
2020-01-19 20:01:19 -0800

    Fixed many oversights: container should have unique_ptrs, num_passengers should be protected so internal_getNumPassengers() can see it. addCar needs to take a unique_ptr<Car> by value, and std::move it into the container.

--- ibrk_car_rental.cpp
+++ ibrk_car_rental.cpp
@@ -8,14 +8,16 @@ class Car
 {
         string plate;
         string brand;
+        virtual unsigned internal_getNumPassengers() const = 0;
+	protected:
         unsigned num_passengers;
-        virtual unsigned internal_getNumPassengers() const { return 0; }
     public: 
 		Car(const string& _p): plate(_p), brand(""), num_passengers(0){};
         Car(const string& _p, const string& _b, unsigned _n): plate(_p), brand(_b), num_passengers(_n) {}
-        string getPlate() const { return plate; }
+		virtual ~Car() = default;
+        const string& getPlate() const { return plate; }
         void setPlate(const string& p) { plate = p; }
-        string getBrand() const { return brand; }
+        const string& getBrand() const { return brand; }
         void setBrand(const string& b) { brand = b; }
         unsigned getNumPassengers() const { return internal_getNumPassengers(); }
 };
@@ -26,7 +28,7 @@ class SUV: public Car
 {
     bool option;
     
-    unsigned internal_getNumPassengers() const { return option ? getNumPassengers() + 3 : getNumPassengers(); }
+    unsigned internal_getNumPassengers() const { return option ? num_passengers + 3 : num_passengers; }
     public:
         SUV(const string& _p, const string& _b, unsigned _n, bool _o): Car(_p,_b,_n), option(_o){}
 };
@@ -34,7 +36,7 @@ class SUV: public Car
 class Sedan: public Car
 {
     bool option;
-    unsigned internal_getNumPassengers() const { return getNumPassengers(); } 
+    unsigned internal_getNumPassengers() const { return num_passengers; } 
 
     public: 
         Sedan(const string& _p, const string& _b, unsigned _n, bool _o): Car(_p,_b,_n), option(_o){}
@@ -42,29 +44,30 @@ class Sedan: public Car
 
 class CarRental
 {
-        unordered_map<string, Car> cars;
+        unordered_map<string, unique_ptr<Car>> cars;
 
     public:
 
-        CarRental(){};
-        CarRental(const unordered_map<string, Car>& _cars) : cars(_cars) {};
-        void addCar(const Car& c) { cars.emplace(make_pair(c.getPlate(), c)); }
-        void removeCar(const Car& c) { cars.erase(c.getPlate());}
+        CarRental() = default;
+        void addCar(unique_ptr<Car> c) { cars[c->getPlate()] = move(c); }
+        void removeCar(const unique_ptr<Car> c) { cars.erase(c->getPlate());}
         size_t numCars() { return cars.size(); } 
-		Car findCar(const string& plate){ return cars.find(plate)->second; }
+		unique_ptr<Car> findCar(const string& plate){ return move(cars.find(plate)->second); }
 
 };
 
 int main(const int argc, char const * const argv[])
 {
 	
-	SUV suv1 { "abc", "Lincoln", 10, true };
-	SUV suv2 { "bcd", "Yukon", 8, false };
-	Sedan sed1 { "cde", "Camry", 5, true };
-	Sedan sed2 { "def", "Continental", 6, false };
+	unique_ptr<SUV> suv1 = make_unique<SUV>( "abc", "Lincoln", 10, true );
+	unique_ptr<SUV> suv2 = make_unique<SUV>( "bcd", "Yukon", 8, false );
+	unique_ptr<Sedan> sed1 = make_unique<Sedan>( "cde", "Camry", 5, true );
+	unique_ptr<Sedan> sed2 = make_unique<Sedan>( "def", "Continental", 6, false );
 
 	CarRental cr;
-	cr.addCar(suv1);
+	cr.addCar(move(suv1));
+	unique_ptr<Car> pc = cr.findCar("abc");
+	cout << "Brand: " << pc->getBrand() << " Passengers: " << pc->getNumPassengers() << endl;
 
     return 0;
 }
