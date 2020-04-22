e4bfbf9b8465e3046902d2b259956cab0170b2ab
carol hill <gcross@licmax.com>
2020-04-22 06:46:12 -0700

    Finally got it all working. Next up, better test driver, more tests, and see what bugs arise.

--- trie.cpp
+++ trie.cpp
@@ -7,29 +7,30 @@
 
 using namespace std;
 
+template<class T>
 class trie
 {
 	struct node
 	{
-		wchar_t val;
-		unordered_map<wchar_t,unique_ptr<node>> children;
-		explicit node(wchar_t _val) : val(_val){};
+		T val;
+		unordered_map<T,unique_ptr<node>> children;
+		explicit node(T _val) : val(_val){};
 	};
 
 	node root;
 
 public:
 	
-	trie() : root(L'า'){};
+	trie() : root(' '){};
 
-	vector<wstring>  match(wstring s)
+	using map_iterator_type = typename unordered_map<T,unique_ptr<node>>::iterator;
+	vector<basic_string<T>>  match(const basic_string<T> & s)
 	{
-		wstring prefix;
+		basic_string<T> prefix;
 		node * n = &root;
-		for (wstring::iterator s_it = s.begin(); s_it < s.end() ; ++s_it)
+		for (typename basic_string<T>::const_iterator s_it = s.begin(); s_it < s.end() ; ++s_it)
 		{
-				
-			unordered_map<wchar_t,unique_ptr<node>>::iterator h_it = n->children.find(*s_it);
+			map_iterator_type h_it = n->children.find(*s_it);
 			if (h_it != n->children.end())
 			{
 				prefix.push_back(*s_it);	
@@ -39,14 +40,14 @@ public:
 					break;
 		}
 			
-		vector<wstring> matches;
-		for (unordered_map<wchar_t,unique_ptr<node>>::iterator it = n->children.begin(); it != n->children.end(); ++it)
+		vector<basic_string<T>> matches;
+		for (map_iterator_type it = n->children.begin(); it != n->children.end(); ++it)
 		{
-			vector<wstring> suffixes = get_suffixes(it->second.get());	
+			vector<basic_string<T>> suffixes = get_suffixes(it->second.get());	
 			transform(suffixes.begin(), 
 				suffixes.end(),
 				back_inserter(matches), 
-				[&prefix](const wstring& suffix)
+				[&prefix](const basic_string<T>& suffix)
 				{
 					return prefix + suffix;		
 				});
@@ -54,15 +55,15 @@ public:
 		return matches;
 	}
 
-	pair<node*,bool>  insert(wstring s)
+	pair<node*,bool>  insert(basic_string<T> const & s)
 	{
 		node * n = &root;
 
-		wstring::const_iterator s_it ;
-		for (s_it= s.begin(); s_it < s.end(); ++s_it)
+		typename basic_string<T>::const_iterator s_it ;
+		for (s_it = s.begin(); s_it < s.end(); ++s_it)
 		{
 
-			unordered_map<wchar_t, unique_ptr<node>>::iterator it = n->children.find(*s_it);
+			map_iterator_type it = n->children.find(*s_it);
 			if (it != n->children.end())
 			{
 				n = it->second.get();
@@ -75,7 +76,7 @@ public:
 		for (;s_it < s.end(); ++s_it)
 		{
 			unique_ptr<node> n_new(make_unique<node>(*s_it));
-			pair<unordered_map<wchar_t, unique_ptr<node>>::iterator, bool> res = n->children.insert(make_pair(*s_it, n_new.release()));
+			pair<map_iterator_type, bool> res = n->children.insert(make_pair(*s_it, n_new.release()));
 			n = res.first->second.get();
 		}
 		return make_pair(n, true);
@@ -84,23 +85,26 @@ public:
 private:
 
 
-	vector<wstring> get_suffixes(node* const n)
+	vector<basic_string<T>> get_suffixes(node* const n)
 	{
-		vector<wstring> suffixes;
+		vector<basic_string<T>> suffixes;
 		if (n->children.empty()) 
 		{
-			// what are we going to do here? 
-			return suffixes.emplace_back(
+			suffixes.emplace_back(1,n->val);
+			return suffixes;
 		}
 
-		wstring s;
-		for (unordered_map<wchar_t,unique_ptr<node>> it = node->children.begin(); 
-			it < node->children.end();
+		basic_string<T> s;
+		vector<basic_string<T>> temp;
+
+		for (map_iterator_type it = n->children.begin(); 
+			it != n->children.end();
 			++it)
 		{
-			vector<wstring> temp = get_suffixes(it->second.get());
-			copy(temp.begin(), temp.end(), back_inserter(suffixes));
+			temp = get_suffixes(it->second.get());
 		}
+		for (basic_string<T>& s: temp) s = n->val + s;
+		copy(temp.begin(), temp.end(), back_inserter(suffixes));
 		return suffixes;
 	}
 };
@@ -109,12 +113,17 @@ int main()
 {
 
 	
-	string s("This is a dog.");
+	string s("bard barstool barraster barn barndoor brindille breezy broach");
 	stringstream sm(s);
 	vector<string> words;
 	copy(istream_iterator<string>(sm), istream_iterator<string>(), back_inserter(words));
 	
 	cout << words.back() << endl;
+
+	trie<char> t;
+	for (string const& w: words) t.insert(w);
+	for (string const& w: t.match("bar")) cout << w << endl;
+	for (string const& w: t.match("br")) cout << w << endl;
 
 	return 0;
 }
