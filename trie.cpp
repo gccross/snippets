506b21d741669e6186f031902a7e3daa653b9c3e
carol hill <gcross@licmax.com>
2020-05-12 05:24:50 -0700

    Updated with John's suggestions for vector<trie<T>> instead of unordered_map.

--- trie.cpp
+++ trie.cpp
@@ -10,42 +10,47 @@ using namespace std;
 template<class T>
 class trie
 {
-	struct node
+	struct node_t
 	{
 		T val;
-		unordered_map<T,unique_ptr<node>> children;
-		explicit node(T _val) : val(_val){};
+		vector<trie<T>> children;
+		explicit node_t(const T&  _val) : val(_val){};
 	};
 
-	node root;
+	unique_ptr<node_t> root;
 
 public:
 	
-	trie() : root(' '){};
+	using iterator_type = typename vector<trie<T>>::iterator;
+	using const_iterator_type = typename vector<trie<T>>::const_iterator;
 
-	using map_iterator_type = typename unordered_map<T,unique_ptr<node>>::iterator;
+	trie() =  default;
+	explicit trie(const T& val): root(make_unique<node_t>(val)) {};
+
+	T& val() const { return root->val; } 
+
 	vector<basic_string<T>>  match(const basic_string<T> & s)
 	{
 		basic_string<T> prefix;
-		node * n = &root;
-		for (typename basic_string<T>::const_iterator s_it = s.begin(); s_it < s.end() ; ++s_it)
+		node_t const* n = root.get();
+		for (typename basic_string<T>::const_iterator s_it = s.cbegin(); s_it < s.cend() ; ++s_it)
 		{
-			map_iterator_type h_it = n->children.find(*s_it);
-			if (h_it != n->children.end())
+			const_iterator_type h_it = lower_bound(n->children.cbegin(), n->children.cend(), *s_it);
+			if (h_it != n->children.cend() && !(*s_it < *h_it)) // value exists
 			{
 				prefix.push_back(*s_it);	
-				n = h_it->second.get();
+				n = h_it->root.get();
 			}
 			else
-					break;
+				break;
 		}
 			
 		vector<basic_string<T>> matches;
-		for (map_iterator_type it = n->children.begin(); it != n->children.end(); ++it)
+		for (const_iterator_type it = n->children.cbegin(); it != n->children.cend(); ++it)
 		{
-			vector<basic_string<T>> suffixes = get_suffixes(it->second.get());	
-			transform(suffixes.begin(), 
-				suffixes.end(),
+			vector<basic_string<T>> suffixes = get_suffixes(it);	
+			transform(suffixes.cbegin(), 
+				suffixes.cend(),
 				back_inserter(matches), 
 				[&prefix](const basic_string<T>& suffix)
 				{
@@ -55,38 +60,41 @@ public:
 		return matches;
 	}
 
-	pair<node*,bool>  insert(basic_string<T> const & s)
+	pair<const_iterator_type,bool>  insert(basic_string<T> const & s)
 	{
-		node * n = &root;
+		if (s.empty()) return(make_pair(vector<trie<T>>().cend(),false));
+		if (!root) root = make_unique<node_t>(s[0]);
+		node_t * n = root.get();
 
 		typename basic_string<T>::const_iterator s_it ;
+		typename vector<trie<T>>::const_iterator it;
 		for (s_it = s.begin(); s_it < s.end(); ++s_it)
 		{
 
-			map_iterator_type it = n->children.find(*s_it);
-			if (it != n->children.end())
-			{
-				n = it->second.get();
-			}
+			it = lower_bound(n->children.cbegin(), n->children.cend(), *s_it);
+			if (it != n->children.cend() && !(*s_it < *it))
+				n = it->root.get();
 			else break;
 		}
 
-		if (s_it == s.end()) return make_pair(n,false);
+		if (s_it == s.end()) return make_pair(it,false);
 
 		for (;s_it < s.end(); ++s_it)
 		{
-			unique_ptr<node> n_new(make_unique<node>(*s_it));
-			pair<map_iterator_type, bool> res = n->children.insert(make_pair(*s_it, n_new.release()));
-			n = res.first->second.get();
+			n->children.emplace_back(*s_it);
+			sort(n->children.begin(), n->children.end()); 
+			it = lower_bound(n->children.cbegin(), n->children.cend(), *s_it);
+			n = it->root.get();
 		}
-		return make_pair(n, true);
+		return make_pair(it, true);
 	}
 
 private:
 
 
-	vector<basic_string<T>> get_suffixes(node* const n)
+	vector<basic_string<T>> get_suffixes(const_iterator_type it)
 	{
+		node_t const* n = it->root.get();
 		vector<basic_string<T>> suffixes;
 		if (n->children.empty()) 
 		{
@@ -97,17 +105,24 @@ private:
 		basic_string<T> s;
 		vector<basic_string<T>> temp;
 
-		for (map_iterator_type it = n->children.begin(); 
-			it != n->children.end();
+		for (const_iterator_type it = n->children.cbegin(); 
+			it != n->children.cend();
 			++it)
 		{
-			temp = get_suffixes(it->second.get());
+			temp = get_suffixes(it);
 		}
 		for (basic_string<T>& s: temp) s = n->val + s;
-		copy(temp.begin(), temp.end(), back_inserter(suffixes));
+		copy(temp.cbegin(), temp.cend(), back_inserter(suffixes));
 		return suffixes;
 	}
 };
+
+template <typename T>
+bool operator< (const trie<T>& l, const trie<T>& r) { return l.val() < r.val(); };
+template <typename T>
+bool operator< (const T& l, const trie<T>& r) { return l < r.val(); };
+template <typename T>
+bool operator< (const trie<T>& l, const T& r) { return l.val() < r; };
 
 int main()
 {
@@ -119,7 +134,6 @@ int main()
 	copy(istream_iterator<string>(sm), istream_iterator<string>(), back_inserter(words));
 	
 	cout << words.back() << endl;
-
 	trie<char> t;
 	for (string const& w: words) t.insert(w);
 	for (string const& w: t.match("bar")) cout << w << endl;
