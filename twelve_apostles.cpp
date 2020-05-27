67d31e902b4a25dd40660294b5d72c90427eb457
carol hill <gcross@licmax.com>
2020-05-27 03:46:31 -0700

    Second implementation. Call vector<>.reserve + vector<>.resize to allocate upfront. Made apostle class copyable. Put call to pthread_cond_signal in an infinite while() loop to avoid infinite wait in pthread_cond_wait().

--- twelve_apostles.cpp
+++ twelve_apostles.cpp
@@ -36,47 +36,46 @@ void* feed(void* data);
 class apostle 
 {
 public: 
-	struct thread_data
-	{
-		vector<apostle> &v;
-		const size_t index;
-	};
-	thread_data td;
 	pthread_t thread;
-	const string & name;
+	string name;
 	unsigned mouthfuls;
 	chrono::seconds sec;
-	chopstick &left, &right;
+	chopstick *left, *right;
 
 	pthread_t& get_thread() { return thread; } 
 
-	apostle(const string &s, chopstick &l, chopstick &r, vector<apostle>& v, const size_t index): 
-		name(move(s)), left(l), right(r), mouthfuls(0), td({v,index})
+	apostle() = default;
+	apostle(const string& a_name, chopstick* a_left, chopstick* a_right): name(a_name), left(a_left), right(a_right){}
+	apostle& operator=(apostle a)
 	{
-		pthread_create(&thread, nullptr, feed, static_cast<void*>( &td));
-	};
+		thread = a.thread;
+		name = a.name;
+		mouthfuls = a.mouthfuls;
+		sec = a.sec;
+		left = a.left;
+		right = a.right;
+		pthread_create(&thread, nullptr, feed, this);
+		return *this;
+	}
 
 };
 
-
-
 void* feed(void* data)
 {
-	const apostle::thread_data & td = *static_cast<apostle::thread_data*>(data);
-	apostle &a = td.v[td.index];  // <-- dangerous, &a could be changed if vector resizes
+	apostle & a = *static_cast<apostle*>(data);
 	while (a.mouthfuls < 20)
 	{
-		pthread_mutex_lock(&a.left.mutex);
-		pthread_cond_wait(&a.left.cond,&a.left.mutex);
-		pthread_mutex_lock(&a.right.mutex);
-		pthread_cond_wait(&a.right.cond,&a.right.mutex);
+		pthread_mutex_lock(&a.left->mutex);
+		pthread_cond_wait(&a.left->cond,&a.left->mutex);
+		pthread_mutex_lock(&a.right->mutex);
+		pthread_cond_wait(&a.right->cond,&a.right->mutex);
 		cout << "Apostle " << a.name << " thread id " << pthread_self() 
 			<< " acquired spoon at count: " << a.mouthfuls << endl;
 		++a.mouthfuls;
-		pthread_cond_signal(&a.left.cond);
-		pthread_cond_signal(&a.right.cond);
-		pthread_mutex_unlock(&a.right.mutex);
-		pthread_mutex_unlock(&a.left.mutex);
+		pthread_mutex_unlock(&a.right->mutex);
+		pthread_cond_signal(&a.right->cond);
+		pthread_mutex_unlock(&a.left->mutex);
+		pthread_cond_signal(&a.left->cond);
 	}
 	return nullptr;
 }
@@ -84,15 +83,18 @@ void* feed(void* data)
 int main (int argc, char const * argv[])
 {
 	array<string, 12> names {"John", "Luke", "Peter", "James", "Judha", "Paul", "Bartholomew", "Matthew", "Philip", "Simon", "Thomas", "Juddhas" };
-	//array<string, 2> names {"John","Luke"};
 	array<chopstick,names.size()> sticks;
 	vector<apostle> apostles;
+	apostles.reserve(names.size());
+	apostles.resize(names.size());
 
 	for (int i=0; i+1<names.size(); ++i)
-		apostles.emplace_back( names[i],sticks[i],sticks[i+1], apostles, i );  // <-- danger!! elements could be relocated elsewhere causing segfault in feed() function
+		apostles[i]=apostle(names[i],&sticks[i],&sticks[i+1]);
 
-	apostles.emplace_back(names[names.size()-1], sticks[0],sticks[names.size()-1], apostles, names.size()-1);
-	for (chopstick& stick: sticks) pthread_cond_signal(&stick.cond);
+	apostles[names.size()-1] = apostle(names[names.size()-1], &sticks[0],&sticks[names.size()-1]);
+	while(true)
+		for (chopstick& stick: sticks)   pthread_cond_signal(&stick.cond); 
+
 	for (apostle&  a: apostles) pthread_join(a.get_thread(), nullptr);
 	
 	return 0;
