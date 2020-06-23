84e977b000bd7e78e5f881f17d0f9e1ee10a3dc1
carol hill <gcross@licmax.com>
2020-06-23 08:40:04 -0700

    Added has_type struct.  A problem from Advertima interview challenge.

--- enable_if.cpp
+++ enable_if.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <type_traits>
+#include <vector>
 using namespace std;
 
 // simple demonstration of only defining constructor if template parameter is an integral 
@@ -16,13 +16,35 @@ struct value
 
 };
 
+template <typename T>
+struct my_trait
+{
+	static const bool value;
+};
+
+template <typename T>
+const bool my_trait<T>::value  = (sizeof(typename T::value_type) != 0);
+
+struct Foo
+{
+	using value_type = int;
+};
+
+struct Bar {};
+
 // a different example to demonstrate forcing compiler message to print enable_if::type
-template <typename T> struct S;
-S<enable_if_t<true>> var;  //compiler error undefined type S<void>
+//template <typename T> struct S;
+//S<enable_if_t<true>> var;  //compiler error undefined type S<void>
 
 int main ()
 {
-	new value(-67);
-	new value("generousity"); //compiler error, "generousity" non-integral type
-	return 0;
+    new value(-67);
+    //new value("generousity"); //compiler error, "generousity" non-integral type
+
+	static_assert(my_trait<std::vector<int>>::value);
+	static_assert(my_trait<Foo>::value);
+	static_assert(my_trait<Bar>::value);
+
+    return 0;
+
 }
