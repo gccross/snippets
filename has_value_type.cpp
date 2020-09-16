72a73b2c97520559a548e0158190cebefb3d4d59
carol hill <gcross@licmax.com>
2020-09-16 12:00:58 -0700

    Added another version, which doesn't use void_t

--- has_value_type.cpp
+++ has_value_type.cpp
@@ -9,6 +9,14 @@ struct  has_value_type: std::false_type {};
 template <typename T>
 struct has_value_type<T, std::void_t<typename T::value_type>>: std::true_type {};
 
+/*
+
+	this works too
+
+template <typename , typename = int> struct has_value_type: std::false_type {};
+template <typename T> struct has_value_type<T,typename T::value_type>: std::true_type {};
+*/ 
+
 int main() 
 { 
 	std::cout << "bool: " << std::boolalpha << has_value_type<bool>::value << std::endl;
